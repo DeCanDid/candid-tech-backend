4e43e4b39c76bae8ef5e835af2c68af1218fb2d2
candid <olacandid@gmail.com>
2025-07-13 21:48:33 +0100

    'fixing-bug'

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["https://candid-tech.netlify.app/"],
+    origin: ["https://candid-tech.netlify.app"]
   })
 );
 app.use(express.json());
